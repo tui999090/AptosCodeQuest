808feb95043d1527dc0bddd92105fc1c949bbb24
tui999090 <sarahjasanpun0suy@hotmail.com>
2024-05-08 16:04:57 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function isPalindrome(str) {
-  return str === str.split("").reverse().join("");
+function pathSum(root, sum) {
+  if (!root) return [];
+  const result = [];
+  const path = [];
+  const traverse = (node, sum) => {
+    if (!node) return;
+    path.push(node.val);
+    if (!node.left && !node.right && sum === node.val) {
+      result.push([...path]);
+    }
+    traverse(node.left, sum - node.val);
+    traverse(node.right, sum - node.val);
+    path.pop();
+  };
+  traverse(root, sum);
+  return result;
 }
