2b9d31d7f769b2266370671f5e0c8fd4b191394b
tui999090 <sarahjasanpun0suy@hotmail.com>
2024-05-09 15:37:29 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function squareDigits(num) {
-  return parseInt(
-    num
-      .toString()
-      .split("")
-      .map((digit) => digit ** 2)
-      .join(""),
-  );
+function canFinish(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
+  }
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
+  let count = 0;
+  while (queue.length) {
+    const course = queue.shift();
+    count++;
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
+    }
+  }
+  return count === numCourses;
 }
