bc46a24d15bc6e0b3ad3ff998aced3d15725e75d
tui999090 <sarahjasanpun0suy@hotmail.com>
2024-05-09 13:13:22 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-function pathSum(root, sum) {
-  if (!root) return [];
-  const result = [];
-  const path = [];
-  const traverse = (node, sum) => {
-    if (!node) return;
-    path.push(node.val);
-    if (!node.left && !node.right && sum === node.val) {
-      result.push([...path]);
-    }
-    traverse(node.left, sum - node.val);
-    traverse(node.right, sum - node.val);
-    path.pop();
-  };
-  traverse(root, sum);
-  return result;
+function squareDigits(num) {
+  return parseInt(
+    num
+      .toString()
+      .split("")
+      .map((digit) => digit ** 2)
+      .join(""),
+  );
 }
