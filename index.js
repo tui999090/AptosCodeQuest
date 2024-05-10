949a0b9493e086489dd2e897439e18ddf9c9e8f2
tui999090 <sarahjasanpun0suy@hotmail.com>
2024-05-11 06:10:45 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-function canFinish(numCourses, prerequisites) {
-  const graph = new Array(numCourses).fill(0).map(() => []);
-  const inDegree = new Array(numCourses).fill(0);
-  for (const [course, pre] of prerequisites) {
-    graph[pre].push(course);
-    inDegree[course]++;
-  }
-  const queue = [];
-  for (let i = 0; i < numCourses; i++) {
-    if (inDegree[i] === 0) queue.push(i);
-  }
-  let count = 0;
-  while (queue.length) {
-    const course = queue.shift();
-    count++;
-    for (const nextCourse of graph[course]) {
-      inDegree[nextCourse]--;
-      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
-    }
-  }
-  return count === numCourses;
-}
+const aliceBalanceAfter = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+  minimumLedgerVersion: BigInt(response.version),
+});
+const bobBalanceAfter = await aptos.getAccountCoinsData({
+  accountAddress: bobAddress,
+});
+const sponsorBalanceAfter = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
