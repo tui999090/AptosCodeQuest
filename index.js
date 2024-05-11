6217fd3466d4059c9bdcb68d3c07372b5b417ddb
tui999090 <sarahjasanpun0suy@hotmail.com>
2024-05-11 08:03:26 +0700

    feat task

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const aliceBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: aliceAddress,
-  minimumLedgerVersion: BigInt(response.version),
-});
-const bobBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: bobAddress,
-});
-const sponsorBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: sponsorAddress,
-});
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const char = num.toString();
+            if (isValid(board, i, j, char)) {
+              board[i][j] = char;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  function isValid(board, row, col, char) {
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === char ||
+        board[i][col] === char ||
+        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
+          3 * Math.floor(col / 3) + (i % 3)
+        ] === char
+      )
+        return false;
+    }
+    return true;
+  }
+}
