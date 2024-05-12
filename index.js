25cf0c2c30ff4dfc1cfe888d264e51a00d6e0772
tui999090 <sarahjasanpun0suy@hotmail.com>
2024-05-13 00:12:10 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-function solveSudoku(board) {
-  solve(board);
-  function solve(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === ".") {
-          for (let num = 1; num <= 9; num++) {
-            const char = num.toString();
-            if (isValid(board, i, j, char)) {
-              board[i][j] = char;
-              if (solve(board)) return true;
-              board[i][j] = ".";
-            }
-          }
-          return false;
-        }
-      }
-    }
-    return true;
+function sortedListToBST(head) {
+  if (!head) return null;
+  if (!head.next) return new TreeNode(head.val);
+  let slow = head;
+  let fast = head;
+  let prev = null;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
   }
-  function isValid(board, row, col, char) {
-    for (let i = 0; i < 9; i++) {
-      if (
-        board[row][i] === char ||
-        board[i][col] === char ||
-        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
-          3 * Math.floor(col / 3) + (i % 3)
-        ] === char
-      )
-        return false;
-    }
-    return true;
-  }
+  const root = new TreeNode(slow.val);
+  prev.next = null;
+  root.left = sortedListToBST(head);
+  root.right = sortedListToBST(slow.next);
+  return root;
 }
